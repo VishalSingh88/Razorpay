beca9e424166fddf0712677d9febaf0927434cf7
VishalSingh88 <vishalrajpurohit@netify.in>
2024-11-22 17:15:52 +0530

    Now, it works.

--- app.py
+++ app.py
@@ -4,17 +4,20 @@ import razorpay
 
 order_id = "nothing"
 
+client = razorpay.Client(auth=("rzp_test_Bs0a1arJo0LHkL","DVurd2KmJ4gGgBk6xAeCOiMI"))
+
 @app.route("/")
 def welcome():
     return "Hello World"
 
-@app.route("/payment", methods=["POST", "GET"])
+@app.route("/payment", methods=["GET"])
 def pay():
     amount = "50000"
-    client = razorpay.Client(auth=("rzp_test_KZwhPzzWj6Rfox","nUlMszQDkMxdNbfWCsIU0sez"))
+    
 
     order = client.order.create({'amount':int(amount), 'currency':'INR', 'payment_capture':'1'})
-    # print(order['id'])
+    print(order)
+    print(order['id'])
     order_id = order['id']
     print(order_id)
     # return order_id
@@ -24,30 +27,40 @@ def pay():
 # def success():
 #     return "Thanks For Payment."
 
-@app.route('/success', methods=["GET",'POST'])
+@app.route('/success', methods=['POST'])
 def payment_success():
-    # payment_id = request.form['razorpay_payment_id']
-    # razorpay_order_id = order_id
-    # signature = request.form['razorpay_signature']
+    paymentId = request.form['razorpay_payment_id']
+    razorpayOrderId = request.form['razorpay_order_id']
+    signature = request.form['razorpay_signature']
+    # razorpayOrderId = razorpayOrderId[:-1] + "r"
 
     # Verify payment signature (refer to Razorpay docs)
-    # params_dict = {
-    #     'razorpay_order_id': razorpay_order_id,
-    #     'razorpay_payment_id': payment_id,
-    #     'razorpay_signature': signature
-    # }
+    paramsDict = {
+        'razorpay_order_id': razorpayOrderId,
+        'razorpay_payment_id': paymentId,
+        'razorpay_signature': signature
+    }
 
-    # print(payment_id," ",razorpay_order_id," ",signature)
-    # client = razorpay.Client(auth=("rzp_test_KZwhPzzWj6Rfox","nUlMszQDkMxdNbfWCsIU0sez"))
-    # client.utility.verify_payment_signature(params_dict)  
+    # return paramsDict
 
-    # print(request.form)
-    # If signature verification is successful, process the payment
-    # razorpay_payment_id
-    # return "Payment successful!"
+    try:
+        verify = client.utility.verify_payment_signature(paramsDict)
+        # Payment successful, process the order here
+        # print("Payment Successful")
+        # print(verify)
+        return "payment successful"
+        # if verify:
+         
+        #     return "payment successful"
+        # elif verify == "Razorpay Signature Verification Failed":
+        #     return "payment failed"
+        
+    except Exception as e:
+        # Payment verification failed
+        # print("Payment Verification Failed")
+        print(e)
+        return "failed"
 
-    print(request)
-    postData = request.form
-    data = postData.to_dict()
-    print(data)
-    return data
+
+if __name__ == '__main__':
+    app.run(debug=True)
